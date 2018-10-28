3d42179b3a6f47ac063a58c011739abc7da2a050
ZhangCheng <gc_zhangcheng@126.com>
2018-10-29 00:38:30 +0800

    多选框

--- src/routes/front/react/MultiSelect.jsx
+++ src/routes/front/react/MultiSelect.jsx
@@ -1,19 +1,98 @@
 import React from 'react';
+import { Tag } from 'antd';
+import styles from './MultiSelect.less';
+import { Icon } from 'antd';
 
+class SelectedTag extends React.PureComponent {
+    render () {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
 
-class MultiSelect extends React.Component {
+class Option extends React.PureComponent {
+    onClick = (e) => {
+        if (this.props.onClick && typeof this.props.onClick === 'function') {
+            this.props.onClick(e);
+        }
+    }
+
     render() {
         return (
-            <select className="full-width" multiple>
-                <option>as</option>
-                <option>b</option>
-                <option>c</option>
-                <option>c</option>
-                <option>c</option>
-                <option>c</option>
-                <option>c</option>
-                <option>c</option>
-            </select>
+            <div className={styles.MultiSelectOption} onClick={e => this.onClick}>
+                <div className={styles.MultiSelectOptionLabel}>
+                    {this.props.children}
+                </div>
+                {
+                    this.props.selected ?
+                        (
+                            <div className={styles.MultiSelectOptionFlag}>
+                                <Icon type="check" theme="outlined" />
+                            </div>
+                        ) : null
+                }
+
+            </div>
+        )
+    }
+}
+
+class MultiSelect extends React.Component {
+    state = {
+        selected: [],
+        options: []
+    }
+
+    shouldComponentUpdate(nextProps) {
+
+    }
+
+    onOptionClick = (value) => {
+        let selected = this.state.selected;
+        let index = selected.indexOf(value);
+        if(index < 0) {
+            selected.push()
+        }else {
+
+        }
+
+        this.setState({selected: selected})
+    }
+
+    render() {
+        let options = [
+            {
+                key: 'a',
+                value: 'a',
+                label: 'a'
+            },
+            {
+                key: 'b',
+                value: 'b',
+                label: 'b'
+            }
+        ]
+
+
+        return (
+            <div className="full-width dropdown" style={{ width: "300px" }}>
+                <div className={"full-width " + styles.MultiSelectTags}>
+                    {
+                        this.state.selected.map(e => {
+                            return <Tag>{e.label}</Tag>
+                        })
+                    }
+                </div>
+                <div className="multiselect-options full-width dropdown-content">
+                    {
+                        options.map(option => {
+                            return <Option onClick={(e) => this.onOptionClick(option.value)} value={option.value} key={option.key} selected={true}>{option.label}</Option>;
+                        })
+                    }
+                </div>
+            </div>
         )
     }
 }
